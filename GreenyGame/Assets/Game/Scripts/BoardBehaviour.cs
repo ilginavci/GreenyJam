f9a0b5de42f21d639018ff28b7624ec889d10363
ilginavci <ilginavci@outlook.com>
2022-10-22 00:39:01 +0300

    Game

---
+++ GreenyGame/Assets/Game/Scripts/BoardBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardBehaviour : MonoBehaviour
+{
+    public Grid[][] _board;
+    public List<Grid> _emptyGrids;
+    public List<Grid> _fullGrids;
+
+}
---
+++ GreenyGame/Assets/Game/Scripts/Grid.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Entity _player;
+    public Entity _item;
+}
